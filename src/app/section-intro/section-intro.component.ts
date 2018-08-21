20524125c64bbd12f24868486c0be67339615a5c
Blake Burgess <blake@stp.co.nz>
2018-08-21 21:00:21 +1200

    Add basic sections

---
+++ src/app/section-intro/section-intro.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section-intro',
+  templateUrl: './section-intro.component.html'
+})
+export class SectionIntroComponent{}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://woah-api.azurewebsites.net'
 };
